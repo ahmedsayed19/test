11fb5e15723e566caafbba4faeabafaba70f5a95
Ahmed Sayed <ahmed@localhost.localdomain>
2023-03-20 16:02:48 -0400

    Thanking feature

--- file.py
+++ file.py
@@ -3,7 +3,9 @@
 def greating(name):
     return f"Hello, {name}"
 
+def thanking(name):
+    return f"Thank you, {name}"
 
 name = input('Enter your name: ')
 
-print(greating(name))
+print(greating(name), thanking(name), sep='\n')
